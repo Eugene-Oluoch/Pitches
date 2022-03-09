1fd2f62a63717644ab9e47a95419f2e4bbdaaecd
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-03-09 15:45:14 +0300

    Added manager

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
-from app import create_app
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User, Pitch
 app = create_app('production')
-app.app_context().push()
+
+manager = Manager(app)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch)
 
 
-
+manager.add_command('server',Server)
 if __name__ == '__main__':
-    app.run()
+    manager.run()
