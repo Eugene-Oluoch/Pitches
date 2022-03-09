c8d27451e2eb2b7b5ef4b0acb6f83b83fdbde1b4
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-03-09 12:55:03 +0300

    Added code in init.py

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from app.config import Config
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    
+    from app.auth.routes import auths
+    from app.main.routes import main
+    from app.errors.handlers import errors
+    app.register_blueprint(auths)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
