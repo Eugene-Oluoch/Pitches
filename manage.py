c52efd5484b96c9ac127afcdba8ab1d59966e33a
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-03-09 15:35:05 +0300

    Deployed

--- manage.py
+++ manage.py
@@ -1,16 +1,8 @@
-from app import create_app,db
-from flask_script import Manager, Server
-from app.models import User, Pitch
-app = create_app('production')
+from app import create_app
+app = create_app('development')
 app.app_context().push()
 
-manager = Manager(app)
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app, db = db, User = User, Pitch = Pitch)
 
 
-manager.add_command('server',Server)
 if __name__ == '__main__':
-    manager.run()
+    app.run()
