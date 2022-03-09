c5604826f8f1b47d978f0a5c089c2bdca36c24bb
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-03-09 23:05:45 +0300

    Change db uri

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL").replace('postgres://', 'posgresql://')
+    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
     
     
 
